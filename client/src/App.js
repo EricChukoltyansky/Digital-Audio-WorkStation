7d193bfd4f9a68f5d29e41efeb6856a892d9c422
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-26 21:57:11 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+import audioFile from "./music.mp3";
+
+
+// let myUrl = "http://localhost:3000"; //development
+
+// if (process.env.NODE_ENV === "production") {
+//   myUrl = "api";
+// }
+// export default axios.create({
+//   baseURL: myUrl,
+// });
+
+const socket = io.connect("http://localhost:3001");
+
+const audio = new Audio();
+
+function App() {
+  const [role, setRole] = useState("");
+  const [playing, setPlaying] = useState("");
+
+  useEffect(() => {
+    const recieveMessage = (m) => {
+      console.log(m);
+      if (role === "ying") {
+        audio.src = m.path;
+        audio.play();
+      }
+      setPlaying(m.name);
+    };
+
+    const stopAudio = () => {
+      setPlaying("");
+    };
+
+    socket.on("play", recieveMessage);
+    socket.on("stop", stopAudio);
+
+    return () => {
+      socket.off("play", { name: "Test sound 1", path: audioFile });
+    };
+  }, [role]);
+
+  useEffect(() => {
+    const handleAudioStop = () => {
+      socket.emit("stop");
+    };
+    audio.addEventListener("pause", handleAudioStop);
+    return () => {
+      audio.removeEventListener("pause", handleAudioStop);
+    };
+  }, []);
+
+  const handlePlaySound = () => {
+    socket.emit("play", { name: "Test sound 1", path: audioFile });
+  };
+
+  return (
+    <div className="App">
+      <h1>Soundbutt</h1>
+      <div>
+        <h4>Role</h4>
+        <button onClick={() => setRole("yang")}>Yang</button>
+        <button onClick={() => setRole("ying")}>Ying</button>
+      </div>
+      <div>
+        <h4>Choose sound</h4>
+        <button onClick={handlePlaySound}>Play Sound!</button>
+      </div>
+      <div>
+        <h4>Playing {playing}</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const path = require("path");
+
+const publicPath = path.join(__dirname, "../client/build");
+
+app.use(cors());
+app.use(express.static(publicPath));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  // socket.on("join_room", (data) => {
+  //   socket.join(data);
+  //   console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  // });
+
+  // socket.on("send_message", (data) => {
+  //   socket.to(data.room).emit("receive_message", data);
+  // });
+
+  socket.on("play", (playMsg) => {
+    io.emit("play", playMsg);
+  });
+
+  socket.on("stop", (stopMsg) => {
+    io.emit("stop");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log("SERVER RUNNING ON PORT 3001");
+});
