6ea3a4e2690a633a6795f48b048dace1e91e31fe
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-28 14:48:15 +0200

    user conflict

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,1 +1,16 @@
-Const User = require('./')
+const User = require("../models/user");
+
+const addUser = async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (e) {
+    res.status.send(e.message);
+  }
+};
+
+module.exports = {
+  addUser,
+};
