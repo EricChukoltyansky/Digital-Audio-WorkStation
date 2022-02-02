f00ad0f3bce35023823385b1084ff58e5a0fb822
EricChukoltyansky <kazohelmet@gmail.com>
2022-02-02 17:22:33 +0200

    controllers function

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,17 +1,49 @@
+// const User = require("../models/user");
 
-const User = require("../models/user");
+// const addUser = async (req, res) => {
+//   const user = new User(req.body);
+//   try {
+//     await user.save();
+//     const token = await user.generateAuthToken();
+//     res.status(201).send({ user, token });
+//   } catch (e) {
+//     res.status.send(e.message);
+//   }
+// };
 
-const addUser = async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    const token = await user.generateAuthToken();
-    res.status(201).send({ user, token });
-  } catch (e) {
-    res.status.send(e.message);
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  const existingUser = users.find((user) => {
+    user.room === room && user.name === name;
+  });
+
+  if (existingUser) {
+    return { error: "Username is taken" };
   }
+
+  const user = { id, name, room };
+
+  users.push(user);
+  return { user };
 };
+
+const removeUser = (id) => {
+  const index = users.filter((user) => {
+    return user.id !== id;
+  });
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
 module.exports = {
   addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
 };
