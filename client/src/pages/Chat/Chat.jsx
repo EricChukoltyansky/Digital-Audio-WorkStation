efcaf4e3c6faba65c15d7a17b144d55385a6d58d
EricChukoltyansky <kazohelmet@gmail.com>
2022-02-02 21:42:04 +0200

    fixing useLocation in the chat component

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,83 +1,86 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import queryString from "query-string";
-// import io from 'socket.io-client';
-import TextContainer from '../../components/TextContainer/TextContainer';
-import Messages from '../../components/Messages/Messages';
-import InfoBar from '../../components/InfoBar/InfoBar';
-import Input from '../../components/Input/Input';
-
+import io from "socket.io-client";
+import TextContainer from "../../components/TextContainer/TextContainer";
+import Messages from "../../components/Messages/Messages";
+import InfoBar from "../../components/InfoBar/InfoBar";
+import Input from "../../components/Input/Input";
 import "./Chat.css";
+import { useLocation } from "react-router-dom";
 
-// var connectionOptions = {
-// 	"force new connection" : true,
-// 	"reconnectionAttempts": "Infinity",
-// 	"timeout" : 10000,				
-// 	"transports" : ["websocket"]
+// let connectionOptions = {
+//   "force new connection": true,
+//   reconnectionAttempts: "Infinity",
+//   timeout: 10000,
+//   transports: ["websocket"],
 // };
 
-// const socket = io.connect('https://localhost:3000',connectionOptions);
+// const socket = io.connect("https://localhost:3001", connectionOptions);
 
+const Chat = ({ socket }) => {
+  console.log(socket);
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
-const Chat = ({location, socket}) => {
+  const ENDPOINT = "localhost:3001";
 
-	const [name, setName] = useState('');
-	const [room, setRoom] = useState("");
-	const [users, setUsers] = useState('');
-	const [message, setMessage] = useState('');
-	const [messages, setMessages] = useState([]);
+  const location = useLocation();
 
-	const ENDPOINT = 'localhost:5000';
+  console.log(socket);
 
-	useEffect( () => {
-		const {name, room} = queryString.parse(location.search);
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    console.log(name, room);
+    setName(name);
+    setRoom(room);
+    socket.emit("join", { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [ENDPOINT, location.search, socket]);
 
-		setName(name);
-		setRoom(room);
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
 
-		socket.emit('join',{name, room}, (error) => {
-			if(error) {
-				alert(error);
-			}
-		})
-		return () => {
-			socket.emit('disconnect');
-			socket.off();
-		}
-		
-	},[ENDPOINT, location.search]);
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, [messages, users]);
 
-	useEffect( () => {
-		socket.on('message', (message) => {
-			setMessages([...messages,message]);
-		})
+  //Function for Sending Message
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
 
-		socket.on("roomData", ({ users }) => {
-			setUsers(users);
-		});
-	},[messages, users])
+  console.log(message, messages);
 
-	//Function for Sending Message
-	const sendMessage = (e) => {
-		e.preventDefault();
-		if(message) {
-			socket.emit('sendMessage', message, () => setMessage(''))
-		}
-	}
-
-	console.log(message ,messages);
-
-	return (
-		<div className="outerContainer">
-			<div className="container">
-		
-				<InfoBar room={room} />
-				<Messages messages={messages} name={name} />
-				<Input message={message} setMessage={setMessage}
-				sendMessage={sendMessage} />
-			</div>
-			<TextContainer users={users}/>
-		</div>
-	)
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
 };
 
 export default Chat;
