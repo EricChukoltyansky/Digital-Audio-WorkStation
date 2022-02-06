7a4b786cd854b11a78dbf300e8e877c87b8ac5ab
EricChukoltyansky <kazohelmet@gmail.com>
2022-02-06 21:09:20 +0200

    add BPM input

---
+++ client/src/components/Sequencer/BPM.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+
+const Style = {
+  BPM: styled.input`
+    -webkit-appearance: none;
+    margin-bottom: 15px;
+    cursor: pointer;
+    width: 15%;
+    border-radius: 20px;
+    direction: rtl;
+
+    &::-webkit-slider-runnable-track {
+      height: 7px;
+      background: #ddd;
+      border: none;
+      border-radius: 5px;
+    }
+
+    &::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      border: none;
+      height: 35px;
+      width: 35px;
+      border-radius: 7px;
+      background: rgb(119, 19, 233);
+      margin-top: -14px;
+      transition: all 0.3s;
+    }
+
+    &::-webkit-slider-thumb:hover {
+      background: rgb(119, 19, 233);
+      border-radius: 20px;
+      cursor: pointer;
+    }
+
+    &:focus {
+      outline: none;
+    }
+  `,
+};
+
+const BPM = ({ max, min, step, type, value, onChange }) => (
+  <Style.BPM
+    max={max}
+    min={min}
+    step={step}
+    type={type}
+    value={value}
+    onChange={onChange}
+  />
+);
+
+export default BPM;
