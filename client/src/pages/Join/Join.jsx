a7246c7f1669d62c3204858ba65820ae684b0cec
EricChukoltyansky <kazohelmet@gmail.com>
2022-02-07 16:54:04 +0200

    finish landing page and enter button

--- client/src/pages/Join/Join.jsx
+++ client/src/pages/Join/Join.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Join.css";
+import "../../assets/fonts/JMH Psychedelic CAPS.otf";
 
 const Join = () => {
   return (
@@ -9,7 +10,7 @@ const Join = () => {
         <h1 className="heading">Join The Revolution</h1>
         <Link to={"/DAW"}>
           <button className={"button mt-20"} type="submit">
-            Click it, it will be fun
+            Click <span>here</span> , it will be fun
           </button>
         </Link>
       </div>
