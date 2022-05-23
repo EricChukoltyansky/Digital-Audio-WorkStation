7b47e725a5d0376aec7d61961235d1881846e2a3
EricChukoltyansky <kazohelmet@gmail.com>
2022-05-23 20:47:31 +0300

    Modal to ts

---
+++ client/src/pages/Modal/ModalPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Modal from "./Modal";
+import useModal from "./useModal";
+
+import "./styles.css";
+
+const ModalPage = () => {
+  const { isShowing, toggle } = useModal();
+  return (
+    <div className="page">
+      <button className="button-default" onClick={toggle}>
+        Instructions
+      </button>
+      <Modal isShowing={isShowing} hide={toggle} />
+    </div>
+  );
+};
+
+export default ModalPage;
