731b9ee1f6a50bc26cabf0f017ba99614bf60eef
EricChukoltyansky <kazohelmet@gmail.com>
2022-05-24 20:12:58 +0300

    addtional git add .

--- client/src/pages/Modal/Modal.jsx
+++ client/src/pages/Modal/Modal.jsx
@@ -30,7 +30,7 @@ const Modal = ({ isShowing, hide }) =>
                 play solo, the best time spent is with your friends. So send the
                 app to your friends, make sure you are on the Sequencer page and
                 start pushing buttons! Both of you should hear live what the
-                other one is pushing!
+                other one is pushing!!!
               </p>
             </div>
           </div>
