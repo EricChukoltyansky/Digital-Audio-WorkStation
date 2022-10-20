6f7ca30dddb50d74eb24d07c9e190f625c0f13f4
Idan Lev <idan.93lev@gmail.com>
2022-10-20 16:59:05 +0300

    functioning right bar

---
+++ client/src/components/buttons/PinkButt.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function PinkButt() {
+  const Butt = styled.button`
+    
+  `
+  return (
+    <div>PinkButt</div>
+  )
+}
