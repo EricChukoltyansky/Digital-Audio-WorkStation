3e5c67007b73b0970b20c4848f502b79c67116da
Idan Lev <idan.93lev@gmail.com>
2022-10-24 18:52:16 +0300

    colorful neon right bar

--- client/src/components/buttons/Instructions.jsx
+++ client/src/components/buttons/Instructions.jsx
@@ -5,6 +5,8 @@ const Button = styled.button`
   border: 1px solid #fff;
   background: transparent;
   color: #fff;
+  box-shadow: 0px 0px 2px #fff;
+  text-shadow: 0px 0px 2px #fff;
   font-size: 1.4rem;
   border-radius: 5px;
   transition: 0.2s ease-in-out;
@@ -12,8 +14,10 @@ const Button = styled.button`
   cursor: pointer;
 
   &:hover {
-    background-color: #f8f8ff;
     color: #000;
+    background-color: #f8f8ff;
+    box-shadow: 0px 0px 5px #fff;
+    text-shadow: 0px 0px 5px #000;
   }
 `;
 
